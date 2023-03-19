7962b90261d95c3dd47ea84dfa76f65d004bf9aa
Pasculi <jorge.sepulveda.turpie@gmail.com>
2023-03-18 23:45:43 -0300

    App Songs

--- songs.js
+++ songs.js
@@ -1,18 +1,36 @@
 let container = document.querySelector(".container");
 let songsContainer = container.querySelector(".songs-container");
-let songs = songsContainer.querySelectorAll(".song");
 let addButton = container.querySelector(".form__submit-btn_action_add");
 let resetButton = container.querySelector(".form__submit-btn_action_reset");
 let image = container.querySelector(".cover__image");
 
-if (songs.length === 0){
-    resetButton.setAttribute("disabled",true);
-    resetButton.style.backgroundColor = "#f1f1f1";
-    resetButton.style.cursor = ("pointer, none");
-}
-else{
-    resetButton.setAttribute("disabled",false);
-}
-console.log(songs.length);
+function renderAdded(){
+    let songs = songsContainer.querySelectorAll(".song");
+    let noSongsElement = container.querySelector(".no-songs");
 
-console.log(songsContainer.className)
+    if (songs.length === 0) {
+      resetButton.setAttribute("disabled", true);
+      resetButton.classList.add("form__submit-btn_disabled");
+      noSongsElement.classList.remove("no-songs_hidden");
+
+
+    } else {
+      resetButton.removeAttribute("disabled", false);
+      resetButton.classList.remove("form__submit-btn_disabled");
+      noSongsElement.classList.add("no-songs_hidden");
+
+    }
+}
+
+function addSong() {
+  songsContainer.innerHTML += `
+    <div class="song">
+          <h4 class="song__artist">The Cars</h4>
+          <p class="song__title">Drive</p>
+          <button class="song__like"></button>
+        </div>
+    `;
+    renderAdded();
+}
+addButton.addEventListener("click", addSong);
+renderAdded();
