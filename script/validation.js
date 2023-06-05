3e8ae2188ab65d1374a1eebb76a83a995fc42597
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2023-06-04 20:23:43 -0400

    feat:creando funciones para validar formularios

--- script/validation.js
+++ script/validation.js
@@ -1,12 +1,38 @@
 const perfil = document.querySelector('.nav__perfil');
 const botonCerrar = document.querySelector('.button__cerrar')
 
-perfil.addEventListener('click', () =>{
-    const modalPerfil = document.querySelector('.grupo__formulario-perfil');
-    modalPerfil.classList.add('grupo__formulario-show');
-})
 
-botonCerrar.addEventListener('click', () =>{
-    const modalPerfil = document.querySelector('.grupo__formulario-perfil');
-    modalPerfil.classList.remove('grupo__formulario-show')
-})
+
+function showModal() {
+    const modalLists = Array.from(document.querySelectorAll('.section__form'));
+    console.log(modalLists)
+    modalLists.forEach((modalList) => {
+        modalList.classList.add('grupo__formulario-show');
+        console.log(modalList)
+    })
+}
+
+
+function closeModal() {
+    const modalLists = Array.from(document.querySelectorAll('.section__form'));
+    modalLists.forEach((modalList) => {
+        modalList.classList.remove('grupo__formulario-show');
+        console.log(modalList)
+    })
+}
+closeModal()
+
+function setEventListeners() {
+    const inputLists = Array.from(document.querySelectorAll('.form__input'));
+    inputLists.forEach((inputList) => {
+        inputList.addEventListener('input',(evt) => {
+            console.log(evt.target.value)
+        })
+    })
+
+}
+
+setEventListeners()
+perfil.addEventListener('click', showModal)
+
+botonCerrar.addEventListener('click', closeModal)
