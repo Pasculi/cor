967f065610ba3cc3ba97a48a64d35ec92867e431
Pasculi <jorge.sepulveda.turpie@gmail.com>
2023-06-04 23:25:30 -0400

    feat:Funcionalidad abrir modal

--- script/validation.js
+++ script/validation.js
@@ -1,38 +1,21 @@
-const perfil = document.querySelector('.nav__perfil');
-const botonCerrar = document.querySelector('.button__cerrar')
 
+/* ABRE EL MODAL CORRESPONDIENTE DESDE EL MENU NAV */
+const elementNavBar = Array.from(document.querySelectorAll(".nav"));
 
+elementNavBar.forEach((navElement) => {
+  navElement.addEventListener("click", (evt) => {
+    const modalElement = document.querySelector(`.section__${evt.target.id}`);
+    console.log(modalElement);
+    modalElement.classList.add('grupo__formulario-show');
+  });
+});
 
-function showModal() {
-    const modalLists = Array.from(document.querySelectorAll('.section__form'));
-    console.log(modalLists)
-    modalLists.forEach((modalList) => {
-        modalList.classList.add('grupo__formulario-show');
-        console.log(modalList)
+/* FUNCIONALIDAD DE CERRAR MODAL */
+const elementClose = Array.from(document.querySelectorAll('.button__cerrar'));
+elementClose.forEach((closeElement)=>{
+    closeElement.addEventListener("click", (evt) => {
+        console.log(evt.target.id)
     })
-}
+})
 
-
-function closeModal() {
-    const modalLists = Array.from(document.querySelectorAll('.section__form'));
-    modalLists.forEach((modalList) => {
-        modalList.classList.remove('grupo__formulario-show');
-        console.log(modalList)
-    })
-}
-closeModal()
-
-function setEventListeners() {
-    const inputLists = Array.from(document.querySelectorAll('.form__input'));
-    inputLists.forEach((inputList) => {
-        inputList.addEventListener('input',(evt) => {
-            console.log(evt.target.value)
-        })
-    })
-
-}
-
-setEventListeners()
-perfil.addEventListener('click', showModal)
-
-botonCerrar.addEventListener('click', closeModal)
+console.log(elementClose);
