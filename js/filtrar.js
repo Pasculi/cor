1d4959c8e0557444dc70163203322d4fa654029b
Pasculi <jorge.sepulveda.turpie@gmail.com>
2023-06-11 22:40:47 -0400

    feat:Crad flip

--- js/filtrar.js
+++ js/filtrar.js
@@ -11,11 +11,11 @@ const buscarCanal = document.querySelector(".form__input");
 const resultados = document.querySelector(".resultado");
 
 
-function mostrarCanales(canales) {
+/* function mostrarCanales(canales) {
   canales.forEach((canal) => {
     resultados.innerHTML += `<p>N°${canal.channel}.- ${canal.nombre} (<strong>ADSL:</strong> ${canal.adsl} - <strong>FCA:</strong> ${canal.fca} - <strong>MPEG4:</strong> ${canal.mpeg4}) - <strong>${canal.criticidad}</strong></p>`;
   })
-}
+} */
 
 
 
