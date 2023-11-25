dd348dfcfeeeff0f8858ecbdfde621893074937f
Rodrigo Turpie <jsepulveda_turpie@hotmail.com>
2023-11-25 11:41:03 -0300

    feat:Eliminacion de archivos

--- canales.js
+++
@@ -1,8 +0,0 @@
-let lista = document.querySelector('.lista-senales');
-let titulo = document.createElement('h1');
-let textoTitulo = document.createTextNode ('Bienvenido a Javascript');
-titulo.appendChild(textoTitulo);
-lista.appendChild(titulo);
-let lista2 = lista.cloneNode(true);
-lista2.style = 'background-color:red';
-lista.insertAdjacentElement('afterbegin', lista2)
